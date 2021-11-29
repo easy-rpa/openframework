18547fbfc2ffc638184c3dcc2f76565b9007bd1c
defcaleon <nikitka.parmon@mail.ru>
2021-11-29 07:34:30 +0300

    update refactoring in GoogleSheets

---
+++ examples/google-sheets/sheets-copying/src/main/java/eu/ibagroup/easyrpa/examples/googlesheets/sheets_copying/tasks/CopySheetBetweenSpreadsheets.java
@@ -0,0 +1,41 @@
+package eu.ibagroup.easyrpa.examples.googlesheets.sheets_copying.tasks;
+
+import eu.ibagroup.easyrpa.engine.annotation.ApTaskEntry;
+import eu.ibagroup.easyrpa.engine.annotation.Configuration;
+import eu.ibagroup.easyrpa.engine.apflow.ApTask;
+import eu.ibagroup.easyrpa.openframework.googlesheets.GoogleSheets;
+import eu.ibagroup.easyrpa.openframework.googlesheets.spreadsheet.Sheet;
+import eu.ibagroup.easyrpa.openframework.googlesheets.spreadsheet.Spreadsheet;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.inject.Inject;
+
+@ApTaskEntry(name = "Copy Sheet between Spreadsheets")
+@Slf4j
+public class CopySheetBetweenSpreadsheets extends ApTask {
+
+    @Configuration(value = "spreadsheet.id.copyFrom")
+    private String spreadsheetIdFrom;
+
+    @Configuration(value = "spreadsheet.id.copyTo")
+    private String spreadsheetIdTo;
+
+    @Configuration(value = "sheet.name")
+    private String sheetName;
+
+    @Inject
+    private GoogleSheets service;
+
+    @Override
+    public void execute() {
+        log.info("Copy sheet '{}' from spreadsheet with id '{}' to '{}'", sheetName, spreadsheetIdFrom, spreadsheetIdTo);
+
+        Spreadsheet spreadsheetFrom = service.getSpreadsheet(spreadsheetIdFrom);
+        Spreadsheet spreadsheetTo = service.getSpreadsheet(spreadsheetIdTo);
+
+        Sheet sourceSheet = spreadsheetFrom.selectSheet(sheetName);
+        service.copySheet(spreadsheetFrom,sourceSheet,spreadsheetTo);
+        log.info("Sheet '{}' has been copied successfully.", sheetName);
+
+    }
+}
