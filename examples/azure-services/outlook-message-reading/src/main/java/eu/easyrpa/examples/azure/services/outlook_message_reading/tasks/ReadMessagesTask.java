81e5d947e48e32c62a1482039ecf51b9a9b1ed8c
Miadzvedzeu_AA <kossart2001@gmail.com>
2022-06-20 23:34:53 +0300

    message sending implementation added

--- examples/azure-services/outlook-message-reading/src/main/java/eu/easyrpa/examples/azure/services/outlook_message_reading/tasks/ReadMessagesTask.java
+++ examples/azure-services/outlook-message-reading/src/main/java/eu/easyrpa/examples/azure/services/outlook_message_reading/tasks/ReadMessagesTask.java
@@ -1,4 +1,19 @@
 package eu.easyrpa.examples.azure.services.outlook_message_reading.tasks;
 
-public class ReadMessagesTask {
+import eu.ibagroup.easyrpa.engine.annotation.ApTaskEntry;
+import eu.ibagroup.easyrpa.engine.apflow.ApTask;
+import services.OutlookEmailService;
+
+import javax.inject.Inject;
+
+@ApTaskEntry(name = "btuh")
+public class ReadMessagesTask extends ApTask {
+
+    @Inject
+    private OutlookEmailService outlookEmailService;
+
+    @Override
+    public void execute() throws Exception {
+        outlookEmailService.sendMail("Bruh","Bruh", "Bruh");
+    }
 }
